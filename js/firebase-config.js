d4ff8b235c37e744a111c5f4ee9687eb579803d6
agbriela <74664893+agbriela@users.noreply.github.com>
2026-07-13 11:47:51 -0300

    Update Firebase config with new credentials
    Updated Firebase configuration with new project details.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,33 @@
 import { initializeApp } from
 "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import {
     getFirestore
 } from
 "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "xxxx",
-    authDomain: "xxxx",
-    projectId: "xxxx",
-    storageBucket: "xxxx",
-    messagingSenderId: "xxxx",
-    appId: "xxxx"
+  apiKey: "AIzaSyAivA8YiJBdVzaVYRx_rh70oTZMwX-BTYs",
+  authDomain: "quiz-rodovias.firebaseapp.com",
+  projectId: "quiz-rodovias",
+  storageBucket: "quiz-rodovias.firebasestorage.app",
+  messagingSenderId: "323537134562",
+  appId: "1:323537134562:web:b9eb2a61d552bb290bcfbb",
+  measurementId: "G-DNSVXJC643"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
