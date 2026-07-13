da71848a7d678a5ee61703eae8a2769ca83ef76c
agbriela <74664893+agbriela@users.noreply.github.com>
2026-07-13 11:27:47 -0300

    Implement participant data submission and redirection
    Added functionality to save participant data to Firestore and redirect.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
+import {
+    db
+} from "./firebase-config.js";
 
+import {
+    collection,
+    addDoc
+} from
+"https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+document
+.getElementById("entrar")
+.addEventListener("click", async () => {
+
+    const nome =
+        document.getElementById("nome").value;
+
+    const equipe =
+        document.getElementById("equipe").value;
+
+    const doc = await addDoc(
+        collection(db, "participantes"),
+        {
+            nome,
+            equipe,
+            pontos: 0
+        }
+    );
+
+    localStorage.setItem(
+        "participante",
+        doc.id
+    );
+
+    location.href =
+        "participante.html";
+});
