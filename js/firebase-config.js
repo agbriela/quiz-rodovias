3dc20c59636d0727eb53a92038eab56be2cc7abb
agbriela <74664893+agbriela@users.noreply.github.com>
2026-07-13 14:24:53 -0300

    Update Firebase SDK to version 12.15.0

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAivA8YiJBdVzaVYRx_rh70oTZMwX-BTYs",
