b2657451340361b5d00b5c1aa1a2f7cd11a7149a
agbriela <74664893+agbriela@users.noreply.github.com>
2026-07-27 16:48:10 -0300

    Refactor Firebase config to use Firestore with cache

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,30 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+    initializeFirestore,
+    memoryLocalCache
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAivA8YiJBdVzaVYRx_rh70oTZMwX-BTYs",
-  authDomain: "quiz-rodovias.firebaseapp.com",
-  projectId: "quiz-rodovias",
-  storageBucket: "quiz-rodovias.firebasestorage.app",
-  messagingSenderId: "323537134562",
-  appId: "1:323537134562:web:b9eb2a61d552bb290bcfbb"
+    apiKey: "AIzaSyAivA8YiJBdVzaVYRx_rh70oTZMwX-BTYs",
+    authDomain: "quiz-rodovias.firebaseapp.com",
+    projectId: "quiz-rodovias",
+    storageBucket: "quiz-rodovias.firebasestorage.app",
+    messagingSenderId: "323537134562",
+    appId: "1:323537134562:web:b9eb2a61d552bb290bcfbb"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(
+    app,
+    {
+        localCache: memoryLocalCache(),
+        experimentalForceLongPolling: true,
+        experimentalLongPollingOptions: {
+            timeoutSeconds: 25
+        }
+    }
+);
